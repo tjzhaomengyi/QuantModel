fd0c753481905885ec4aacf2bad337880bce8a43
mikemyzhao <tjzhaomengyi@163.com>
2024-01-05 22:00:26 +0800

    【凑合用版本】暂时有个RNN版本，可以预测持仓天数
    结合策略来看凑合能用，后面可以炼丹，或者进行小模型的升级迭代，算完看着还行

--- datamodel/prepare_data/prepare_data.py
+++ datamodel/prepare_data/prepare_data.py
@@ -5,7 +5,7 @@ import pandas as pd
 # from keras.layers import Dense
 from pytdx.hq import TdxHq_API
 from pandas.tseries.offsets import BDay
-from datetime import datetime
+from datetime import datetime, timedelta
 from retrying import retry
 from ast import literal_eval
 import ast
@@ -42,7 +42,8 @@ def get_history_min_data(row):
     return data
 
 
-def split_min_data(row, tag, start_index, end_index):
+#DNN处理方法，在里面求平均值
+def split_min_data_DNN(row, tag, start_index, end_index):
     min_info = row['min_info']
     if min_info ==  None:
         print(row['stockid'],row['buytime'])
@@ -59,7 +60,28 @@ def split_min_data(row, tag, start_index, end_index):
         print(row._name, row['stockid'],row['buytime'])
     return avg
 
+#RNN方法，对数据进行保留
+def split_min_data_RNN(row, tag):
+    min_info = row['min_info']
+    if min_info == None:
+        print(row['stockid'], row['buytime'])
+        return
+    try:
+        min_info = min_info.replace('OrderedDict', 'collections.OrderedDict')
+        min_info_arr = eval(min_info)
+        res_arr = [ele[tag] for ele in min_info_arr]
+    except:
+        print(row._name, row['stockid'], row['buytime'])
+    return res_arr
 
+
+def make_timestamp(row):
+    buy_date = row['buytime'].split(' ')[0]
+    start_time = datetime.strptime(buy_date + " 09:30:00", '%Y-%m-%d %H:%M:%S')
+    min_prc = row['min_prc']
+    min_prcs_cnt = len(min_prc)
+    timestamps = [(start_time + timedelta(minutes=i)).strftime('%Y-%m-%d %H:%M:%S') for i in range(min_prcs_cnt)]
+    return timestamps
 
 
 
@@ -68,7 +90,7 @@ def get_history_day_data(row):
     if (row._name + 1) % 50 == 0:
         print("日线数据拉取数据到：" + str(row._name) + "行")
     code = row['stockid'].split('.')[1]
-    buy_date = row['buytime'].split(' ')[0]
+    buy_date = str(row['buytime']).split(' ')[0]
     start_date = trade_dates[trade_dates.index(buy_date) - 3]
     feature_data = {}
     with api.connect('119.147.212.81', 7709):
@@ -127,8 +149,8 @@ def parse_json_array(json_str):
 # print(history[1]['vol'])
 
 #1、读取mysql的原始数据，增加分钟数据
-'''
-data = pd.read_csv('data_min.csv')
+''''
+data = pd.read_csv('/home/zhaomengyi/Projects/QuantProjects/QuantDataMiner/datamodel/prepare_data/res/410001005971/410001005971.csv')
 print(data.columns)
 data['trade_diff'] = data.apply(lambda row: trading_days_diff(row), axis=1)
 data['min_info'] = data.apply(lambda row: get_history_min_data(row), axis=1)
@@ -137,26 +159,19 @@ data.to_csv('data_min.csv', index=False, sep=',', header=True)
 '''
 '''
 #2、在分钟数据上添加日线数据和财务数据
+data = pd.read_csv('/home/zhaomengyi/Projects/QuantProjects/QuantDataMiner/datamodel/prepare_data/res/410001005971/data_min.csv')
 data['day_info'] = data.apply(lambda row: get_history_day_data(row), axis=1)
 data['liutongguben'] = data.apply(lambda row:get_finance_data(row, 'liutongguben'), axis=1)
 data.to_csv('data_full_info.csv', index=False, sep=',', header=True)
 '''
-
-data = pd.read_csv('/home/zhaomengyi/Projects/QuantProjects/QuantDataMiner/datamodel/prepare_data/data_full_info.csv',
+data = pd.read_csv('/home/zhaomengyi/Projects/QuantProjects/QuantDataMiner/datamodel/prepare_data/res/410030031937/data_full_info.csv',
                    escapechar=None, converters={'min_info':parse_ordered_dict_array})
 # data['min_info'] = data['min_info'].apply(parse_ordered_dict_array)
-#3、将min_info分成8组，每3个一组
-for i in range(8):
-    prc_tag = 'min_prc_' + str(i)
-    vol_tag = 'min_vol_' + str(i)
-    start_index = i * 30
-    end_index = i * 30 + 29
-    data[prc_tag] = data.apply(lambda row:split_min_data(row, 'price', start_index, end_index), axis=1)
-    data[vol_tag] = data.apply(lambda row:split_min_data(row, 'vol', start_index, end_index), axis=1)
 
-data = data.drop('min_info', axis=1)
 
-#data['day_info']的数据类型时字典，key为日线上各种指标，value是近四天指标的具体数值
+
+
+#3_1data['day_info']的数据类型时字典，key为日线上各种指标，value是近四天指标的具体数值
 data['day_info'] = data['day_info'].apply(literal_eval).apply(dict)
 #技巧：将字典拆成多列，以day_vol为例子，拆成day_vol_0道day_vol_3
 data = data['day_info'].apply(pd.Series).merge(data, left_index=True, right_index=True)
@@ -170,7 +185,25 @@ data[['day_vol_' + str(i) for i in range(4)]] = data['vol'].apply(pd.Series)
 for i in range(4):
     data['day_hsl_' + str(i)] =  data['day_vol_' + str(i)] / data['liutongguben']
 
-data = data.drop('day_info', axis=1)
+#3_2方法1：将min_info分成8组，每3个一组,DNN处理方法，把一天的分钟数据都打在8条八小时数据中
+'''
+for i in range(8):
+    prc_tag = 'min_prc_' + str(i)
+    vol_tag = 'min_vol_' + str(i)
+    start_index = i * 30
+    end_index = i * 30 + 29
+    data[prc_tag] = data.apply(lambda row:split_min_data_DNN(row, 'price', start_index, end_index), axis=1)
+    data[vol_tag] = data.apply(lambda row:split_min_data_DNN(row, 'vol', start_index, end_index), axis=1)
+'''
+#3_2方法2：RRN时序处理方法，将240个分钟直接暴力打散
+data['min_prc'] = data.apply(lambda row:split_min_data_RNN(row, 'price'), axis=1)
+data['min_vol'] = data.apply(lambda row:split_min_data_RNN(row, 'vol'), axis=1)
+data['timestamp'] = data.apply(lambda row:make_timestamp(row), axis=1)
+data = data.explode(['min_prc', 'min_vol', 'timestamp'])
+
+
+
+
+data = data.drop(['day_info','min_info'], axis=1)
 pd.set_option('display.max_columns', None)  # 显示所有列
 data.to_csv('train_data.csv', index=False, sep=',', header=True)
-
